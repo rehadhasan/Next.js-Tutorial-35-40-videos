79c5f7fa5fad0f4676708012700d77d05f2b86b5
rehadhasan <rehad.dev@gmail.com>
2025-02-16 08:12:15 +0600

    36-40 videos done

---
+++ src/app/comments/route.ts
@@ -0,0 +1,18 @@
+import {comments} from "@/app/comments/data";
+
+export async function GET(){
+    return Response.json(comments)
+}
+
+export async function POST(request: Request){
+    const comment = await request.json();
+    const newComment = {
+        id: `${comments.length +1}`,
+        text: comment.text,
+    }
+    comments.push(newComment)
+    return new Response(JSON.stringify(comments),{
+        headers: {"Content-Type": "application/json"},
+        status: 201,
+    })
+}
